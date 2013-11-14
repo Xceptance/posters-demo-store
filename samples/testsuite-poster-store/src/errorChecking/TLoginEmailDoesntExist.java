a701df6b5309ecdb10629eaf570eef027cdb6136
Sebastian Loob <s.loob@xceptance.net>
2013-11-14 15:00:04 +0100

    generated JUnit wrapper class for test cases

---
+++ samples/testsuite-poster-store/src/errorChecking/TLoginEmailDoesntExist.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package errorChecking;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Verifies that an error is shown if the user wants to log in with an email that doesn't exist.
+ */
+@ScriptName
+("errorChecking.TLoginEmailDoesntExist")
+public class TLoginEmailDoesntExist extends AbstractScriptTestCase
+{
+}
---
+++ samples/testsuite-poster-store/src/samples/TOrder.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package samples;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * Simulates browsing the catalog, adding product(s) to the cart, checkout as registered customer and place the order.
+ */
+@ScriptName
+("samples.TOrder")
+public class TOrder extends AbstractScriptTestCase
+{
+}
