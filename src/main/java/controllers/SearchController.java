091b9427995233ee4326ab02fd93e2b8c99782bf
Sebastian Loob <s.loob@xceptance.net>
2014-01-06 13:44:30 +0100

    adjust search to new paging

--- src/main/java/controllers/SearchController.java
+++ src/main/java/controllers/SearchController.java
@@ -1,19 +1,15 @@
 package controllers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import models.Product;
-import models.SubCategory;
 import ninja.Context;
 import ninja.Result;
 import ninja.Results;
 import ninja.i18n.Messages;
 import ninja.params.Param;
-import ninja.params.PathParam;
-import util.database.CarouselInformation;
 import util.database.CommonInformation;
 
 import com.avaje.ebean.Ebean;
@@ -38,59 +34,14 @@ public class SearchController
 
     private Optional language = Optional.of("en");
 
-    public Result search(@Param("searchText") String searchText, Context context)
-    {
-        final Map<String, Object> data = new HashMap<String, Object>();
-        CommonInformation.setCommonData(data, context, xcpConf);
-        String template;
-        // search text is empty
-        if (searchText.isEmpty() || searchText.equals(" "))
-        {
-            template = xcpConf.templateIndex;
-            context.getFlashCookie().put("info", msg.get("infoNoSearchTerm", language).get());
-            CarouselInformation.addCarouselProductsToMap(data);
-        }
-        else
-        {
-            final List<Product> products = new ArrayList<Product>();
-            // search products, which contain search text in the name
-            mergeProductLists(products, searchProductName(searchText));
-            // search products, which contain search text in the short description
-            mergeProductLists(products, searchProductShortDescription(searchText));
-            // search products, which contain search text in the long description
-            mergeProductLists(products, searchProductLongDescription(searchText));
-            // search products, which contain search text in category they belong to
-            mergeProductLists(products, searchCategory(searchText));
-            // search text contains at least two words
-            if (searchText.contains(" "))
-            {
-                // divide search text by spaces
-                String[] searchTerms = searchText.split(" ");
-                for (String searchTerm : searchTerms)
-                {
-                    mergeProductLists(products, searchProductName(searchTerm));
-                    mergeProductLists(products, searchProductShortDescription(searchText));
-                    mergeProductLists(products, searchProductLongDescription(searchText));
-                    mergeProductLists(products, searchCategory(searchTerm));
-                }
-            }
-            data.put("products", products);
-
-            if (products.isEmpty())
-            {
-                data.put("noResults", msg.get("noSearchResults", language).get());
-            }
-            else
-            {
-                data.put("searchText", msg.get("searchProductMatch", language).get() + " '" + searchText + "'");
-            }
-            template = xcpConf.templateProductOverview;
-        }
-        return Results.html().render(data).template(template);
-    }
-
-    public Result search2(@Param("searchText") String searchText, @PathParam("pageNumber") int pageNumber,
-                          Context context)
+    /**
+     * Returns a product overview page with products, that matches the search text.
+     * 
+     * @param searchText
+     * @param context
+     * @return
+     */
+    public Result searchProduct(@Param("searchText") String searchText, Context context)
     {
         // search text is empty
         if (searchText.isEmpty() || searchText.trim().isEmpty())
@@ -100,38 +51,8 @@ public class SearchController
         }
         else
         {
-            // build SQL string
-            String sql = "SELECT id, name, url, minimum_Price, description_detail FROM product where ";
-            // divide search text by spaces
-            String[] searchTerms = searchText.split(" ");
-            // search in description detail
-            sql += "LOWER(description_detail) LIKE LOWER('%" + searchTerms[0] + "%')";
-            // search in name
-            sql += " OR LOWER(name) LIKE LOWER('%" + searchTerms[0] + "%')";
-            if (searchTerms.length > 1)
-            {
-                // add next search term to select statement
-                for (int i = 1; i < searchTerms.length; i++)
-                {
-                    sql += " OR LOWER(description_detail) LIKE LOWER('%" + searchTerms[i] + "%')";
-                    sql += " OR LOWER(name) LIKE LOWER('%" + searchTerms[i] + "%')";
-                }
-            }
-            // create SQL statement
-            RawSql rawSql = RawSqlBuilder.parse(sql).create();
-            Query<Product> query = Ebean.find(Product.class);
-            query.setRawSql(rawSql);
-            int pageSize = xcpConf.pageSize;
-            // get paging list
-            final PagingList<Product> pagingList = query.findPagingList(pageSize);
-            // get all products to 
-            int totalProductCount = query.findList().size();            
-            // get row count in background
-            pagingList.getFutureRowCount();
-            // get the current page
-            Page<Product> page = pagingList.getPage(pageNumber - 1);
-            // get the products of the current page
-            List<Product> products = page.getList();
+            final Map<String, Object> data = new HashMap<String, Object>();
+            List<Product> products = searchForProducts(searchText, 1, data);
             // no product was found
             if (products.isEmpty())
             {
@@ -141,87 +62,94 @@ public class SearchController
             // at least one product was found
             else
             {
-                final Map<String, Object> data = new HashMap<String, Object>();
-                CommonInformation.setCommonData(data, context, xcpConf);
-                // add the products to the data map
                 data.put("products", products);
-                // add the page count to the data map
-                data.put("totalPages", Math.ceil(totalProductCount / (double)pageSize));
+                CommonInformation.setCommonData(data, context, xcpConf);
                 data.put("searchText", msg.get("searchProductMatch", language).get() + " '" + searchText + "'");
-                data.put("currentPage", pageNumber);
-                data.put("isSearch", "true");
                 data.put("searchTerm", searchText);
+                data.put("currentPage", 1);
                 return Results.html().render(data).template(xcpConf.templateProductOverview);
             }
         }
     }
 
     /**
-     * Returns a list of products, which contain the search text in the name.
+     * Returns a list of products, that matches the search text.
      * 
      * @param searchText
+     * @param pageNumber
+     * @param context
      * @return
      */
-    protected List<Product> searchProductName(String searchText)
+    public Result getProductOfSearch(@Param("searchText") String searchText, @Param("page") int pageNumber,
+                                     Context context)
     {
-        return Ebean.find(Product.class).where().icontains("name", searchText).findList();
-    }
-
-    /**
-     * Returns a list of products, which contain the search text in the long description.
-     * 
-     * @param searchText
-     * @return
-     */
-    protected List<Product> searchProductLongDescription(String searchText)
-    {
-        return Ebean.find(Product.class).where().icontains("descriptionDetail", searchText).findList();
-    }
-
-    /**
-     * Returns a list of products, which contain the search text in the short description.
-     * 
-     * @param searchText
-     * @return
-     */
-    protected List<Product> searchProductShortDescription(String searchText)
-    {
-        return Ebean.find(Product.class).where().icontains("descriptionOverview", searchText).findList();
-    }
-
-    /**
-     * Returns a list of products, which contain the search text in the category they belong to.
-     * 
-     * @param searchText
-     * @return
-     */
-    protected List<Product> searchCategory(String searchText)
-    {
-        List<Product> products = new ArrayList<Product>();
-        List<SubCategory> categories = Ebean.find(SubCategory.class).where().icontains("name", searchText).findList();
-        for (SubCategory category : categories)
+        final Map<String, Object> data = new HashMap<String, Object>();
+        List<Product> products = searchForProducts(searchText, pageNumber, data);
+        for (int i = 0; i < products.size(); i++)
         {
-            products.addAll(Ebean.find(Product.class).where().eq("subCategory", category).findList());
+            products.get(i).setAvailableSizes(null);
+            products.get(i).setSubCategory(null);
+            products.get(i).setTopCategory(null);
+            products.get(i).setBasket(null);
+            products.get(i).setOrder(null);
         }
+        data.put("products", products);
+        return Results.json().render(data);
+    }
+
+    /**
+     * Returns a list of products, that matches the search text.
+     * 
+     * @param searchText
+     * @param pageNumber
+     * @param data
+     * @return
+     */
+    private List<Product> searchForProducts(String searchText, int pageNumber, final Map<String, Object> data)
+    {
+        // build SQL string
+        String sql = "SELECT id, name, url, minimum_Price, description_detail FROM product where ";
+        // divide search text by spaces
+        String[] searchTerms = searchText.split(" ");
+        // search in description detail
+        sql += "LOWER(description_detail) LIKE LOWER('%" + searchTerms[0] + "%')";
+        // search in name
+        sql += " OR LOWER(name) LIKE LOWER('%" + searchTerms[0] + "%')";
+        if (searchTerms.length > 1)
+        {
+            // add next search term to select statement
+            for (int i = 1; i < searchTerms.length; i++)
+            {
+                sql += " OR LOWER(description_detail) LIKE LOWER('%" + searchTerms[i] + "%')";
+                sql += " OR LOWER(name) LIKE LOWER('%" + searchTerms[i] + "%')";
+            }
+        }
+        // create SQL statement
+        RawSql rawSql = RawSqlBuilder.parse(sql).create();
+        Query<Product> query = Ebean.find(Product.class);
+        query.setRawSql(rawSql);
+        int pageSize = xcpConf.pageSize;
+        // get paging list
+        PagingList<Product> pagingList = query.findPagingList(pageSize);
+        // get all products to
+        int totalProductCount = query.findList().size();
+        // get row count in background
+        pagingList.getFutureRowCount();
+        // get the current page
+        Page<Product> page = pagingList.getPage(pageNumber - 1);
+        // get the products of the current page
+        List<Product> products = page.getList();
+        // remove some data of the product list, to render a small-sized json-object
+        for (int i = 0; i < products.size(); i++)
+        {
+            products.get(i).setAvailableSizes(null);
+            products.get(i).setSubCategory(null);
+            products.get(i).setTopCategory(null);
+            products.get(i).setBasket(null);
+            products.get(i).setOrder(null);
+        }
+        // add the page count to the data map
+        data.put("totalPages", Math.ceil(totalProductCount / (double) pageSize));
         return products;
-    }
-
-    protected static void mergeProductLists(final List<Product> products, final List<Product> productsToAdd)
-    {
-        for (Product productToAdd : productsToAdd)
-        {
-            boolean isInList = false;
-            for (int i = 0; i < products.size(); i++)
-            {
-                if (products.get(i).getId() == productToAdd.getId())
-                {
-                    isInList = true;
-                }
-            }
-            if (!isInList)
-            {
-                products.add(productToAdd);
-            }
-        }
     }
 }
