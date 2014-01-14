f1d5e793cc089437da3ece156fdc822432bbe72c
Sebastian Loob <s.loob@xceptance.net>
2014-01-14 16:37:18 +0100

    adjust start script

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 #setup paths and options
 CURR_DIR=`pwd`
 JVM_OPTS="-Dsun.lang.ClassLoader.allowArraySyntax=true "
-POSTER_OPTS="-Dstarter.home=$CURR_DIR -Dninja.external.configuration=application.conf -Dninja.port=9000 -Dninja.mode=prod $@"
+POSTER_OPTS="-Dstarter.home=$CURR_DIR -Dninja.external.configuration=conf/application.conf -Dninja.port=9000 -Dninja.mode=prod $@"
 POSTER_JAR="demo-poster-store-1.0.0-SNAPSHOT-jar-with-dependencies.jar"
 
 # run with all options
-java $JVM_OPTS $POSTER_OPTS -jar $POSTER_JAR
+java -Dninja.port=8080 -jar $POSTER_JAR
