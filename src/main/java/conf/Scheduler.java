78dae069c720b5fbde64da0cc1392d437fcf32fb
Sebastian Loob <s.loob@xceptance.net>
2014-01-14 17:17:57 +0100

    add comments in Scheduler

--- src/main/java/conf/Scheduler.java
+++ src/main/java/conf/Scheduler.java
@@ -15,22 +15,22 @@ public class Scheduler
 {
 
     /**
-     * Deletes unused baskets.
+     * Deletes unused carts.
      */
     @Schedule(delay = 3600, timeUnit = TimeUnit.SECONDS)
-    public void deleteUnusedBasket()
+    public void deleteUnusedCart()
     {
         List<Basket> baskets = BasketInformation.getAllBaskets();
-        // check each basket
+        // check each cart
         for (Basket basket : baskets)
         {
-            // just delete if basket belongs to no customer
+            // just delete if cart belongs to no customer
             if (basket.getCustomer() == null)
             {
-                // delete basket, if last update is more than 3600 seconds ago
+                // delete cart, if last update is more than 3600 seconds ago
                 boolean delete = true;
                 List<Basket_Product> basketProducts = basket.getProducts();
-                // check each product of the basket
+                // check each product of the cart
                 for (Basket_Product basketProduct : basketProducts)
                 {
                     if (basketProduct.getLastUpdate().getTime() + (long) 3600000 > System.currentTimeMillis())
