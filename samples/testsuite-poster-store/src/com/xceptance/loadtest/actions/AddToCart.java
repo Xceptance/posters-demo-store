935bca9c6abc8bdb91e247b06361f925b2fc8d7b
sauer-peters <s.sauer-peters@xceptance.de>
2014-01-24 17:15:41 +0100

    Small refactorings and additional comments

--- samples/testsuite-poster-store/src/com/xceptance/loadtest/actions/AddToCart.java
+++ samples/testsuite-poster-store/src/com/xceptance/loadtest/actions/AddToCart.java
@@ -16,32 +16,28 @@ public class AddToCart extends AbstractHtmlPageAction
 {
 
     /**
-     * The 'Add to cart' button
-     */
-    HtmlElement addToCartButton;
-    
-    /**
      * The ID of the product to add to cart
      */
     String productId;
-    
+
     /**
      * The selected poster size
      */
     String size;
-    
+
     /**
      * The selected poster finish (matte or gloss)
      */
     String finish;
-    
+
     /**
      * The 'Add to cart' form
      */
     HtmlForm addToCartForm;
-    
+
     /**
-     * 	Constructor
+     * Constructor
+     * 
      * @param previousAction
      * @param timerName
      */
@@ -52,67 +48,80 @@ public class AddToCart extends AbstractHtmlPageAction
 
     @Override
     public void preValidate() throws Exception
-    {       
-        // Get the result of the last action
-        final HtmlPage page = getPreviousAction().getHtmlPage();
-	
-        // Look up the add to cart form
-        addToCartForm = HtmlPageUtils.findSingleHtmlElementByID(page, "addToCartForm");
-	
-        // Configure the product by selecting a random finish (matte or gloss).
+    {
+	// Get the result of the last action
+	final HtmlPage page = getPreviousAction().getHtmlPage();
+
+	// Look up the 'add to cart' form
+	addToCartForm = HtmlPageUtils.findSingleHtmlElementByID(page,
+		"addToCartForm");
+
+	// Configure the product by selecting a random finish (matte or gloss).
 	HtmlPageUtils.checkRadioButtonRandomly(addToCartForm, "finish");
-	// FIXME: extract the real selected finish 
+	// FIXME: extract the real selected finish
 	finish = "matte";
-	
-	// We choose one of the size options randomly and remember it as a string.
-	// We will need it as a parameter later on in the subsequent AJAX calls to update price and add product to cart
-	HtmlElement option = HtmlPageUtils.findHtmlElementsAndPickOne(page, "id('selectSize')/option");
+
+	// We choose one of the size options randomly and remember it as a
+	// string.
+	// We will need it as a parameter later on in the subsequent AJAX calls
+	// to update price and add poster to cart
+	HtmlElement option = HtmlPageUtils.findHtmlElementsAndPickOne(page,
+		"id('selectSize')/option");
+	// Get the text content of the element as string
 	size = option.getTextContent();
-	
+
 	// Get the product ID. This is also needed for the AJAX calls
-	productId = HtmlPageUtils.findSingleHtmlElementByXPath(page, "id('main')/div/div[2]/div[@class='row-fluid']").getAttribute("productid");
-	 
-        // Look up the 'Add to cart' button
-        addToCartButton = HtmlPageUtils.findSingleHtmlElementByID(page, "btnAddToCart");
+	productId = HtmlPageUtils.findSingleHtmlElementByXPath(page,
+		"id('main')/div/div[2]/div[@class='row-fluid']").getAttribute(
+		"productid");
+
+	// Check the presence of the add to cart button (even though we do not use
+	// it since we're just simulating the AJAX calls that are normally
+	// triggered by JavaScript after hittin gthe button)
+	HtmlPageUtils.isElementPresent(page,"id('btnAddToCart')");
 
     }
 
     @Override
     protected void execute() throws Exception
     {
-        // Get the result of the last action
-        final HtmlPage page = getPreviousAction().getHtmlPage();
-        
-	// With JavaScript disabled due to performance reasons on the load generating agents... 
+	// Get the result of the last action
+	final HtmlPage page = getPreviousAction().getHtmlPage();
+
+	// With JavaScript disabled due to performance reasons on the load
+	// generating agents...
 	getWebClient().getOptions().setJavaScriptEnabled(false);
-	// ... we need to simulate the AJAX calls that update the price after selecting a size and add the product to cart.
-	
-        // Update price:
-	// First we collect the request (POST) parameters for the call and create a list of name value pairs, one for each parameter
+	// ... we need to simulate the AJAX calls that update the price after
+	// selecting a size and add the poster to cart.
+
+	// Update price:
+	// First we collect the request (POST) parameters for the call and
+	// create a list of name value pairs, one for each parameter
 	List<NameValuePair> updatePriceParams = new ArrayList<NameValuePair>();
 	updatePriceParams.add(new NameValuePair("productId", productId));
 	updatePriceParams.add(new NameValuePair("size", size));
-	
+
 	// Perform the AJAX call and return the result.
-	WebResponse updatePriceResponse = AjaxUtils.callPost(page, "/updatePrice", updatePriceParams);
-	
-        // Put the resulting price into the page
-        // TODO
-        
-        // Get cart element slider content before adding poster to cart
-        // Perform the AJAX call and return the result.
-        WebResponse cartElementSliderResponse = AjaxUtils.callGet(page, "/getCartElementSlider", null);
-        
-        
-        // Add poster to cart:
-	// First we collect the request parameters for the call and create a list of name value pairs, one for each parameter
+	WebResponse updatePriceResponse = AjaxUtils.callPost(page,
+		"/updatePrice", updatePriceParams);
+
+	// TODO Analyse JSON. Put the returned price into the page.
+
+	// Get cart element slider content before adding poster to cart
+	// Perform the AJAX call and return the result.
+	// This call doesn't have any parameters
+	WebResponse cartElementSliderResponse = AjaxUtils.callGet(page,
+		"/getCartElementSlider", null);
+
+	// Add poster to cart:
+	// Collect the request parameters
 	List<NameValuePair> addToCartParams = new ArrayList<NameValuePair>();
 	addToCartParams.add(new NameValuePair("productId", productId));
 	addToCartParams.add(new NameValuePair("finish", finish));
 	addToCartParams.add(new NameValuePair("size", size));
-	
+
 	// Perform the AJAX call and return the result.
-	AjaxUtils.callGet(page, "/addToCartSlider", addToCartParams);
+	WebResponse addToCartResponse = AjaxUtils.callGet(page, "/addToCartSlider", addToCartParams);
 
 	// Publish the results.
 	setHtmlPage(page);
@@ -124,8 +133,5 @@ public class AddToCart extends AbstractHtmlPageAction
 	// TODO Auto-generated method stub
 
     }
-    
-    
-
 
 }
