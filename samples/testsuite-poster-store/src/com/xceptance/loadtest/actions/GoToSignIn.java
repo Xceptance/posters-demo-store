eb8c15010626734c80f15fb410532400ddf00c24
Sebastian Loob <s.loob@xceptance.net>
2014-02-03 16:54:00 +0100

    added TRegister

---
+++ samples/testsuite-poster-store/src/com/xceptance/loadtest/actions/GoToSignIn.java
@@ -0,0 +1,71 @@
+package com.xceptance.loadtest.actions;
+
+import org.junit.Assert;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.xceptance.loadtest.validators.HeaderValidator;
+import com.xceptance.xlt.api.actions.AbstractHtmlPageAction;
+import com.xceptance.xlt.api.util.HtmlPageUtils;
+import com.xceptance.xlt.api.validators.ContentLengthValidator;
+import com.xceptance.xlt.api.validators.HtmlEndTagValidator;
+import com.xceptance.xlt.api.validators.HttpResponseCodeValidator;
+
+/**
+ * This {@link AbstractHtmlPageAction} opens the sign in page.
+ * 
+ * @author sebastianloob
+ */
+public class GoToSignIn extends AbstractHtmlPageAction
+{
+
+    /**
+     * The sign in button.
+     */
+    private HtmlElement signInButton;
+
+    public GoToSignIn(AbstractHtmlPageAction previousAction, String timerName)
+    {
+        super(previousAction, timerName);
+    }
+
+    @Override
+    public void preValidate() throws Exception
+    {
+        // Get the result of the last action
+        final HtmlPage page = getPreviousAction().getHtmlPage();
+
+        // check that no customer is logged
+        Assert.assertTrue("A customer is already logged.",
+                          HtmlPageUtils.isElementPresent(page, "id('btnShowLoginForm')"));
+
+        // remember the sign in button
+        this.signInButton = HtmlPageUtils.findSingleHtmlElementByID(page, "btnShowLoginForm");
+    }
+
+    @Override
+    protected void execute() throws Exception
+    {
+        // load the page to sign in
+        loadPageByClick(this.signInButton);
+    }
+
+    @Override
+    protected void postValidate() throws Exception
+    {
+        // get the result of the last action
+        final HtmlPage page = getHtmlPage();
+
+        // Basic checks - see action 'Homepage' for some more details how and when to use these validators
+        HttpResponseCodeValidator.getInstance().validate(page);
+        ContentLengthValidator.getInstance().validate(page);
+        HtmlEndTagValidator.getInstance().validate(page);
+
+        HeaderValidator.getInstance().validate(page);
+
+        // check that it's the sign in page
+        Assert.assertTrue("Sign in form not found.", HtmlPageUtils.isElementPresent(page, "id('formLogin')"));
+        Assert.assertTrue("Link to register not found.", HtmlPageUtils.isElementPresent(page, "id('linkRegister')"));
+    }
+
+}
---
+++ samples/testsuite-poster-store/src/com/xceptance/loadtest/actions/Logout.java
@@ -0,0 +1,75 @@
+package com.xceptance.loadtest.actions;
+
+import org.junit.Assert;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.xceptance.loadtest.validators.HeaderValidator;
+import com.xceptance.xlt.api.actions.AbstractHtmlPageAction;
+import com.xceptance.xlt.api.util.HtmlPageUtils;
+import com.xceptance.xlt.api.validators.ContentLengthValidator;
+import com.xceptance.xlt.api.validators.HtmlEndTagValidator;
+import com.xceptance.xlt.api.validators.HttpResponseCodeValidator;
+
+/**
+ * This {@link AbstractHtmlPageAction} logs out the currently logged in user.
+ * 
+ * @author sebastianloob
+ */
+public class Logout extends AbstractHtmlPageAction
+{
+
+    /**
+     * The log out link.
+     */
+    HtmlElement logout;
+
+    public Logout(AbstractHtmlPageAction previousAction, String timerName)
+    {
+        super(previousAction, timerName);
+    }
+
+    @Override
+    public void preValidate() throws Exception
+    {
+        // get the result of the last action
+        final HtmlPage page = getPreviousAction().getHtmlPage();
+
+        // check that the customer is logged
+        Assert.assertTrue("No customer is logged.", HtmlPageUtils.isElementPresent(page, "id('headerLoggedCustomer')"));
+
+        // remember log out link
+        this.logout = HtmlPageUtils.findSingleHtmlElementByXPath(page, "id('btnLogout')/a");
+    }
+
+    @Override
+    protected void execute() throws Exception
+    {
+        // log out
+        loadPageByClick(logout);
+    }
+
+    @Override
+    protected void postValidate() throws Exception
+    {
+        // get the result of the last action
+        final HtmlPage page = getHtmlPage();
+
+        // Basic checks - see action 'Homepage' for some more details how and when to use these validators
+        HttpResponseCodeValidator.getInstance().validate(page);
+        ContentLengthValidator.getInstance().validate(page);
+        HtmlEndTagValidator.getInstance().validate(page);
+
+        HeaderValidator.getInstance().validate(page);
+
+        // check that no customer is logged
+        Assert.assertTrue("A customer is still logged.", HtmlPageUtils.isElementPresent(page, "id('btnShowLoginForm')"));
+
+        // check that it's the home page
+        final HtmlElement blogNameElement = page.getHtmlElementById("titleIndex");
+        Assert.assertNotNull("Title not found", blogNameElement);
+        // check the title
+        Assert.assertEquals("Title does not match", "Check out our new panorama posters!", blogNameElement.asText());
+    }
+
+}
