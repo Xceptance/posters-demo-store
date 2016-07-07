32ea7a3cf7a52510d8b191a3f06949ff56cc2c07
choelbing <c.hoelbing@xcpetance.net>
2016-07-07 17:09:47 +0200

    adjust card slider
    Signed-off-by: choelbing <c.hoelbing@xcpetance.net>

--- src/main/java/assets/js/customCartSlider.js
+++ src/main/java/assets/js/customCartSlider.js
@@ -1,11 +1,15 @@
 function showCartSlider(show) {
-	if (show === true) {
-		$("#slidingTopWrap").clearQueue().attr('style', 'display:none;')
+	
+	getCartSliderText();
+	
+	/*if (show === true) {
+	/ *	$("#slidingTopWrap").clearQueue().attr('style', 'display:none;')
 				.slideDown("slow", function() {
 					if ($("#slidingTopWrap").is(":visible")) {
 						getCartSliderText();
 					}
-				});
+				});* /
+		getCartSliderText();
 	} else if (show === false) {
 		$("#slidingTopWrap").clearQueue().slideUp("slow").style;
 	} else {
@@ -14,7 +18,7 @@ function showCartSlider(show) {
 				getCartSliderText();
 			}
 		});
-	}
+	}*/
 }
 
 function getCartSliderText() {
@@ -27,21 +31,25 @@ function updateCartSlider(data) {
 	$('#cartSliderElementList').empty();
 	// add products to list
 	for (var i = 0; i < data.cartElements.length; i++) {
-		var liId = "productId" + data.cartElements[i].productId
+		
+		/*var liId = "productId" + data.cartElements[i].productId
 				+ data.cartElements[i].finish + data.cartElements[i].size.width
 				+ "x" + data.cartElements[i].size.height;
+		*/
 		var inner = setCartSliderElementInnerHtml(data.cartElements[i],
 				data.currency, data.unitLength);
-		var liElement = $("<li id='" + liId + "'></li>").html(inner);
+		var liElement = $("<li class='cartSliderElementListItems'></li>").html(inner);
 		$("#cartSliderElementList").append(liElement);
 	}
+
 	// update total price in cart slider
-	$('#cartSliderSubOrderPrice').text(data.currency + data.subOrderTotal);
+	$('#cartMiniTotalSubOrderPrice').text(data.currency + data.subOrderTotal);
+//	$('#cartProductCount').text(data.currency + data.subOrderTotal);
 }
 
 function setCartSliderElementInnerHtml(product, currency, unitLength) {
 
-	return '<ul class="list-unstyled">' + '<li class="prodName">'
+	return '<ul class="cartItems list-unstyled">' + '<li class="prodName">'
 			+ product.productName + '</li>'
 			+ '<li>Quantity: <span class="prodCount">' + product.productCount + '</span>'
 			+ ' (<span class="prodStyle">' + product.finish
@@ -54,6 +62,8 @@ function setCartSliderElementInnerHtml(product, currency, unitLength) {
 
 function addToCart(productId, finish, size) {
 	getCartSliderText();
+	addToCartSlider(productId, finish, size);
+	/*
 	if ($("#slidingTopWrap").is(":visible")) {
 		addToCartSlider(productId, finish, size);
 	} else {
@@ -61,7 +71,7 @@ function addToCart(productId, finish, size) {
 				.slideDown("slow", function() {
 					addToCartSlider(productId, finish, size);
 				}).delay(2000).slideUp("slow");
-	}
+	}*/
 }
 
 function addToCartSlider(productId, finish, size) {
@@ -86,28 +96,43 @@ function addToCartSlider(productId, finish, size) {
 			} else {
 				$("#cartSliderElementList").prepend(liElement);
 			}
-			// update total price in cart slider
-			$('#cartSliderSubOrderPrice').text(
+			// update total sub Order Price
+			$('#cartMiniTotalSubOrderPrice').text(
 					data.currency + data.subOrderTotal);
 			$("#notificationsLoader").empty();
-			// update cart in header
-			$("#headerCartOverview span.CartOverview").text(
+			
+			// update cart in header (counter)
+			$("#headerCartOverview span.headerCartProductCount").text(
 					data.headerCartOverview);
+			//update product counter in mini-cart
+			$("#cartMiniWrap span.cartMiniCartProductCounter").text(
+					data.headerCartOverview);
+			
 		}
 	});
 }
 
 $(document).ready(function() {
-	$("#cartSliderItemsWrap li:first").hide();
-	$("#slidingTopWrap").hide();
+	showCartSlider();
+/*	$("#cartSliderItemsWrap li:first").hide();
+
 
 	$('#cartSliderItemsWrap').mouseover(function() {
 		showCartSlider(true);
 	});
-	$('#btnCartOverview').mouseenter(function() {
-		showCartSlider(true);
+	$('#main').click(function() {
+		$('#searchHeaderForm').css('display','none');
+		$('#header-search-trigger').css('display','block');
 	});
 	$('#btnCartOverview').mouseleave(function() {
 		showCartSlider(false);
+	});*/
+	$('#header-search-trigger').click(function() {
+		$('#searchHeaderForm').css('display','block');
+		$('#header-search-trigger').css('display','none');
 	});
+	$('#btnSearch').click(function() {
+		$('#searchHeaderForm').css('display','none');
+		$('#header-search-trigger').css('display','block');
+	});	
 });
