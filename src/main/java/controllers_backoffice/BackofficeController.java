39aba4b013d107c0ae1d3f44b8cc815881eb3ce9
Kenny <Kennyftw2@gmail.com>
2023-05-09 16:11:16 +0200

    added the Order table in the backoffice

--- src/main/java/controllers_backoffice/BackofficeController.java
+++ src/main/java/controllers_backoffice/BackofficeController.java
@@ -1,9 +1,13 @@
 package controllers_backoffice;
 
 import java.util.Optional;
+import java.util.List;
+
+import com.avaje.ebean.Ebean;
 
 
 import filters.SessionUserExistFilter;
+import models.Order;
 import ninja.Context;
 import ninja.FilterWith;
 import ninja.Result;
@@ -30,7 +34,14 @@ public class BackofficeController {
      */
     @FilterWith(SessionUserExistFilter.class)
     public Result homepage(final Context context)
-    {
-        return Results.html();
+    {   
+        Result result = Results.html();
+        // Find all of the orders
+        List<Order> orders = Ebean.find(Order.class).findList();
+        // Add orders into the back office
+        System.out.println(orders);
+        System.out.println("----kenny-----");
+        result.render("orders", orders);
+        return result;
     }
 }
