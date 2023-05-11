3a28bb798164531872d587f418648024e9aa8248
Kenny <Kennyftw2@gmail.com>
2023-05-11 13:25:36 +0200

    added customer table and current user in backoffice

--- src/main/java/controllers_backoffice/BackofficeController.java
+++ src/main/java/controllers_backoffice/BackofficeController.java
@@ -8,10 +8,13 @@ import com.avaje.ebean.Ebean;
 
 import filters.SessionUserExistFilter;
 import models.Order;
+import models.Customer;
+import models_backoffice.User;
 import ninja.Context;
 import ninja.FilterWith;
 import ninja.Result;
 import ninja.Results;
+import util.session.SessionHandling;
 
 
 /**
@@ -39,9 +42,16 @@ public class BackofficeController {
         // Find all of the orders
         List<Order> orders = Ebean.find(Order.class).findList();
         // Add orders into the back office
-        System.out.println(orders);
-        System.out.println("----kenny-----");
         result.render("orders", orders);
+
+        // Find all of the customers
+        List<Customer> customers = Ebean.find(Customer.class).findList();
+        // Add orders into the back office
+        result.render("customers", customers);
+
+        // Find current user
+        User currentUser = Ebean.find(User.class, SessionHandling.getUserId(context));
+        result.render("currentUser", currentUser);
         return result;
     }
 }
