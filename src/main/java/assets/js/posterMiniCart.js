cbaf9d1549a88fa6d63d91bb4839ac6f2fb7b5ff
Elias <e.thiele@xceptance.com>
2024-02-19 11:16:54 +0100

    Update posterMiniCart.js
    switched all JQuery to vanilla JS

--- src/main/java/assets/js/posterMiniCart.js
+++ src/main/java/assets/js/posterMiniCart.js
@@ -12,40 +12,36 @@ function showMiniCart() {
 
 function getMiniCartText() {
 
-	$(".notificationsLoader").html(
-			'<img src="' + CONTEXT_PATH + '/assets/img/loader.gif">');
-	$.ajax({
-		type : "GET",
-		url : CONTEXT_PATH + '/getMiniCartElements',
-		success : function(data) {
-			$('.cartMiniElementList').empty();
+	document.querySelector(".notificationsLoader").innerHTML = ['<img src="' + CONTEXT_PATH + '/assets/img/loader.gif">'];
+	var url = CONTEXT_PATH + '/getMiniCartElements';
+	const xhr = new XMLHttpRequest();
 
-			/* start with the last product*/
+	xhr.onreadystatechange = function(data) {
+		if(xhr.readyState !== 4) return;
+
+		if(xhr.status === 200) {
+			var minicartElementList = document.querySelector('.cartMiniElementList')
+			minicartElementList.innerHTML = [''];
+
 			for (var i = (data.productsInCartList.length-1); i >= 0; i--) {
-
-				// create new <li> element
-				var inner = getMiniCartElementInnerHtml(
-						data.productsInCartList[i], data.currency,
-						data.unitLength);
-				var liElement = $(
-						"<li class='cartMiniProductsListItems'></li>")
-						.html(inner);
-
-				$(".cartMiniElementList").prepend(liElement);
-
+				var minicartElement = document.createElement("li");
+				minicartElement.classList.add('cartMiniProductsListItems');
+				minicartElement.innerHTML = getMiniCartElementInnerHtml(
+					data.productsInCartList[i], data.currency,
+					data.unitLength);
+				minicartElementList.append(minicartElement);
 			}
 
-			// update product counter in mini-cart
-			$(".cartMiniProductCounter span.value").text(
-					data.cartProductCount);
-			// update total sub Order Price
-			$('.subOrderPrice').text(
-					data.currency + data.subTotalPrice);
+			document.querySelector(".cartMiniProductCounter span.value").textContent = data.cartProductCount;
+			document.querySelector('.subOrderPrice').textContent = data.currency + data.subTotalPrice;
 
-			$(".notificationsLoader").empty();
+			document.querySelector(".notificationsLoader").innerHTML = [''];
+		} else {
+			sendAlert("failed to update mini cart", "alert-danger");
 		}
-	});
-
+	}
+	xhr.open("GET", url);
+	xhr.send();
 }
 
 /* add product to cart */
@@ -53,46 +49,42 @@ function addToCart(productId, finish, size) {
 	addToMiniCart(productId, finish, size);
 }
 function addToMiniCart(productId, finish, size) {
-	$(".notificationsLoader").html(
-			'<img src="' + CONTEXT_PATH + '/assets/img/loader.gif">');
-	$.ajax({
-		type : "GET",
-		url : CONTEXT_PATH + '/addToCartSlider' + '?productId=' + productId
-				+ '&finish=' + finish + '&size=' + size,
-		success : function(data) {
-			// create new <li> element
-			var liId = "productId" + data.product.productId + data.product.finish + data.product.size.width + "x" + data.product.size.height;
-			var inner = getMiniCartElementInnerHtml(data.product, data.currency, data.unitLength);
-			var liElement = $("<li class='" + liId +  " miniCartItem'" + "></li>").html(inner);
-			
+	document.querySelector(".notificationsLoader").innerHTML = ['<img src="' + CONTEXT_PATH + '/assets/img/loader.gif">'];
+	var url = CONTEXT_PATH + '/addToCartSlider' + '?productId=' + productId + '&finish=' + finish + '&size=' + size;
+	const xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function(data) {
+		if(xhr.readyState !== 4) return;
+
+		if(xhr.status === 200) {
 			/* remove existing product*/
-			$("." + liId).remove();				
+			document.querySelector("." + liId).remove();
 			/* replace */
-			$(".cartMiniElementList").prepend(liElement);
+			minicartEntry = document.createElement("li");
+			minicartEntry.classList.add("'" + liId +  " miniCartItem'");
+			minicartEntry.innerHTML = getMiniCartElementInnerHtml(data.product, data.currency, data.unitLength);
 			/* update total sub Order Price */
-			$('.subOrderPrice').text(
-					data.currency + data.subOrderTotal);
+			document.querySelector('.subOrderPrice').textContent = data.currency + data.subOrderTotal;
 			/* notification off */
-			$(".notificationsLoader").empty();
+			document.querySelector(".notificationsLoader").innerHTML = [''];
 
 			/* update cart in header (counter) */
-			$("#headerCartOverview span.headerCartProductCount").text(
-					data.headerCartOverview);
+			document.querySelector("#headerCartOverview span.headerCartProductCount").textContent = data.headerCartOverview;
 			/* update product counter in mini-cart */
-			$(".cartMiniProductCounter span.value").text(
-					data.headerCartOverview);
-
+			document.querySelector(".cartMiniProductCounter span.value").textContent = data.headerCartOverview;
 			/* show minicart and than hide after 1500 ms */
-			$('.top-menu #headerCartOverview').attr('aria-expanded', 'true').addClass('show');
-            $('.top-menu #mini-cart-menu').addClass('show');
+			document.querySelector(".top-menu #headerCartOverview").classList.add('show');
+			document.querySelector(".top-menu #mini-cart-menu").classList.add('show');
 			window.setTimeout(function(){
-			$('.top-menu #headerCartOverview').attr('aria-expanded', 'false').parent().removeClass('show');
-			$('.top-menu #mini-cart-menu').removeClass('show');
-            }, 1500);
-
-
+				document.querySelector(".top-menu #headerCartOverview").classList.remove('show');
+				document.querySelector(".top-menu #mini-cart-menu").classList.remove('show');
+			}, 1500);
+		} else {
+			sendAlert("failed to update mini cart", "alert-danger");
 		}
-	});
+	}
+	xhr.open("GET", url);
+	xhr.send();
 }
 
 /* return a (html)list with all products from the cart*/
