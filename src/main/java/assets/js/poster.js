6dfb2816559ceb599016b98e64ff585f64e2d1ee
Elias <e.thiele@xceptance.com>
2024-02-21 14:31:09 +0100

    Update poster.js
    replaced all JQuery with vanilla JS

--- src/main/java/assets/js/poster.js
+++ src/main/java/assets/js/poster.js
@@ -100,67 +100,72 @@ function updateProductOverview(data) {
 }
 
 //Setup on DOM ready
-$(document).ready(function() {
- if ($('body').hasClass('process')) {
-        $('.process').style.width = "33%";
-        }
-	$('#header-search-trigger').click(function() {
-		$('#header-menu-search').css('display', 'block');
-		$('#header-search-trigger').css('display', 'none');
+function postersSetup() {
+	if(document.body.classList.contains('process')) {
+		document.querySelector('.process').style.width = "33%";
+	}
+	document.getElementById('header-search-trigger').addEventListener('click', () => {
+		document.getElementById('header-menu-search').style.display = 'block';
+		document.getElementById('header-search-trigger').style.display = 'none';
 	});
-	$('#btnSearch').click(function() {
-		$('#header-menu-search').css('display', 'none');
-		$('#header-search-trigger').css('display', 'block');
+	document.getElementById('btnSearch').addEventListener('click', () => {
+		document.getElementById('header-menu-search').style.display = 'block';
+		document.getElementById('header-search-trigger').style.display = 'none';
 	});
-
 	//Sync search input of mobile and not mobile
-	$("#s").on("keyup paste", function() {
-    	$("#sMobile").val($(this).val());
+	document.getElementById('s').addEventListener("keyup paste", () => {
+		document.getElementById('sMobile').value = this.value;
 	});
-	$("#sMobile").on("keyup paste", function() {
-    	$("#s").val($(this).val());
+	document.getElementById('sMobile').addEventListener("keyup paste", () => {
+		document.getElementById('s').value = this.value;
 	});
-
 	//Check if searchterm is Empty
-	$('#searchForm').submit(function(e){
-		var input = $('#s').val().trim();
+	document.getElementById('searchForm').submit(function(){
+		var input = document.getElementById('s').value.trim();
 		if (!input){
-			$('#s').val(input);
+			document.getElementById('s').value = input;
 			return false;
 		}
 	})
-
 	//Setup click handler for update and delete button
-	if ($('#deleteProductModal').length){
-		$('.btnUpdateProduct').click(function(){
-			var cartProductId = $(this).data('id');
-			var cartIndex = $(this).data('index');
-			var inputValue = $('#productCount' + cartIndex).val();
+	if (document.getElementById('deleteProductModal').length) {
+		document.querySelector('.btnUpdateProduct').addEventListener('click', () => {
+			var cartProductId = this.data('id');
+			var cartIndex = this.data('index');
+			var inputValue = document.querySelector('#productCount' + cartIndex).value;
+
 			if (inputValue == 0){
-				$(this).siblings('.btnRemoveProduct').click();
+				this.siblings('.btnRemoveProduct').click();
 			}
-			else{
-				$('#productCount' + cartIndex).prop("defaultValue", inputValue);
-				updateProductCount(cartProductId, inputValue, cartIndex);
+			else {
+				document.querySelector('#productCount' + cartIndex).prop("defaultVaule", inputValue);
+				updateProductCount(cartProductId, inputValue, cartIndex)
 			}
 		});
 
 		//Setup handler for buttons of Confimation popup
-		$('#deleteProductModal').on('show.bs.modal', function(e){
-			var cartProductId = $(e.relatedTarget).data('id');
-			var cartIndex = $(e.relatedTarget).data('index');
-			var prodInfo = $(e.relatedTarget).closest('tr.cartOverviewProduct').find('.productInfo').clone();
-			var oldValue = $('#productCount' + $(e.relatedTarget).data('index')).prop("defaultValue");
+		document.getElementById('deleteProductModal').addEventListener('show.bs.modal', (e) => {
+			var cartProductId = document.querySelector(e.relatedTarget).data('id');
+			var cartIndex = document.querySelector(e.relatedTarget).data('index');
+			var prodInfo = document.querySelector(e.relatedTarget).closest('tr.cartOverviewProduct').find('.productInfo').clone();
+			var oldValue = document.querySelector('#productCount' + document.querySelector(e.relatedTarget).data('index')).prop("defaultValue");
 			//Rollback the input value if close button clicked
-			$('#buttonClose').click(function(){
-				$('#productCount' + cartIndex).val(oldValue);
+			document.getElementById('buttonClose').addEventListener('click', () => {
+				document.querySelector('#productCount' + cartIndex).value = oldValue;
 			});
 			//Delete button
-			$(this).find('.btn-danger').click(function(){
+			this.querySelectorAll(`:scope ${'.btn-danger'}`).addEventListener('click', () => {
 				deleteFromCart(cartProductId, cartIndex);
-			})
+			});
 			//Bodycontent of popup
-			$(this).find('.modal-body').html(prodInfo);			
+			this.querySelectorAll(`:scope ${'.modal-body'}`).innerHTML = prodInfo;
 		});
-	}	
-});
+	}
+}
+function ready(postersSetup) {
+	if (document.readyState !== 'loading') {
+	  postersSetup();
+	} else {
+	  document.addEventListener('DOMContentLoaded', postersSetup);
+	}
+}  
