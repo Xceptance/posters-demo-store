c5110936c1a6836385975c123e61842cabe624d2
Elias <e.thiele@xceptance.com>
2024-10-08 13:25:07 +0200

    Update WebShopController.java
    removed unnecessary data entry for lang, this is already included in freemarker per default and set by the framework (ninja)

--- src/main/java/controllers/WebShopController.java
+++ src/main/java/controllers/WebShopController.java
@@ -97,8 +97,6 @@ public class WebShopController
      */
     public static void setCommonData(final Map<String, Object> data, final Context context, final PosterConstants xcpConf)
     {
-        // set language
-        data.put("lang", "en-US");
         // set categories
         data.put("topCategory", TopCategory.getAllTopCategories());
         // get cart by session
