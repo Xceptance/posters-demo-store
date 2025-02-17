302e370c68ea443b869bf1af22a10e1b2857d5c8
Elias <e.thiele@xceptance.com>
2025-02-17 12:14:46 +0100

    Update SearchController.java
    added load of localized message.properties file and added localized Buy Here button text to data map to make it available to JS

--- src/main/java/controllers/SearchController.java
+++ src/main/java/controllers/SearchController.java
@@ -15,11 +15,14 @@
  */
 package controllers;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 
 import io.ebean.DB;
 import io.ebean.PagedList;
@@ -111,9 +114,34 @@ public class SearchController
      */ // data.put("productCounter", products.size());
     public Result getProductOfSearch(@Param("searchText") final String searchText, @Param("page") final int pageNumber,
             final Context context, @PathParam("urlLocale") String locale) {
+
         final Map<String, Object> data = new HashMap<String, Object>();
+
         // search for products
         final List<Product> products = searchForProducts(searchText, pageNumber, data, locale);
+
+        // load the messages.properties file for the current locale to supply localized texts to JavaScript
+        Properties mesprop = new Properties();
+        try {
+            InputStream input = new FileInputStream("./src/main/java/conf/messages_"+locale+".properties");
+            mesprop.load(input);
+            // text for buy here button
+            data.put("BuyText", mesprop.getProperty("buttonBuyHere"));
+            input.close();
+        } catch (Exception e) {
+            // The file could not be loaded or does not exist. Fall back to the default
+            try {
+                InputStream input = new FileInputStream("./src/main/java/conf/messages.properties");
+                mesprop.load(input);
+                // text for buy here button
+                data.put("BuyText", mesprop.getProperty("buttonBuyHere"));
+                input.close();
+            } catch (Exception e2) {
+                // Fall back to a hardcoded version
+                data.put("BuyText", "Buy Here");
+            }
+        }
+
         // set some attributes to null to get a small-sized JSON
         for (int i = 0; i < products.size(); i++) {
             products.get(i).setAvailableSizes(null);
@@ -122,8 +150,10 @@ public class SearchController
             products.get(i).setCart(null);
             products.get(i).setOrder(null);
         }
+
         data.put("products", products);
         data.put("currentPage", pageNumber);
+
         return Results.json().render(data);
     }
 
