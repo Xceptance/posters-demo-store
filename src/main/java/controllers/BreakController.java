1b7c94fd05725a0803953f0e48e57b90d7537129
Elias <e.thiele@xceptance.com>
2025-04-01 14:40:17 +0200

    Update BreakController.java
    implemented the controllers' handling of the new configuration options and slightly changed implementation for better scalability

--- src/main/java/controllers/BreakController.java
+++ src/main/java/controllers/BreakController.java
@@ -12,8 +12,12 @@ import ninja.Results;
 import ninja.Context;
 import ninja.i18n.Messages;
 import ninja.params.PathParam;
-import ninja.params.Param;
 
+/*
+ * A controller handling the ability to demonstrate wrong site behaviour.
+ * It connects the 'secret' page to configure how the shop should behave to the class storing that information.
+ * The actual breaking of functionalities is realized by AdjustStateFilter.
+ */
 public class BreakController
 {
         
@@ -37,20 +41,43 @@ public class BreakController
     }
 
     // save changes made to configuration and then redirect to status info
-    public  Result statusUpdate(final Context context, @PathParam("urlLocale") String locale, @Param("breakCategories") String breakCategories)
+    public  Result statusUpdate(final Context context, @PathParam("urlLocale") String locale)
     {
         final Map<String, Object> data = new HashMap<String, Object>();
         WebShopController.setCommonData(data, context, xcpConf);
 
-        if (breakCategories!=null && breakCategories.equals("on"))
+        // get the choices made in the submitted form
+        String breakCategories = context.getParameter("breakCategories");
+        String breakCartQuantities = context.getParameter("breakCartQuantities");
+        String blockOrders = context.getParameter("blockOrders");
+        String breakSearch = context.getParameter("breakSearch");
+        String mixUpAddToCart = context.getParameter("mixUpAddToCart");
+        String openLogin = context.getParameter("openLogin");
+        String breakOrderHistory = context.getParameter("breakOrderHistory");
+        String breakLocalization = context.getParameter("breakLocalization");
+
+        // save the desired configuration
+        stsConf.setCategoriesBroken(isSet(breakCategories));
+        stsConf.setCartQuantitiesChange(isSet(breakCartQuantities));
+        stsConf.setOrdersBlocked(isSet(blockOrders));
+        stsConf.setSearchResultsChanged(isSet(breakSearch));
+        stsConf.setCartProductMixups(isSet(mixUpAddToCart));
+        stsConf.setOpenLogin(isSet(openLogin));
+        stsConf.setOrderHistoryMessy(isSet(breakOrderHistory));
+        stsConf.setWrongLocale(isSet(breakLocalization));
+        
+        return Results.redirect(context.getContextPath() + "/" + locale + "/ok3ok2ru8udqx7gZGS9n/statusInfo");
+    }
+
+    private boolean isSet(String configurationChoiceParameter)
+    {
+        if (configurationChoiceParameter!=null && configurationChoiceParameter.equals("on"))
         {
-            stsConf.setCategoriesBroken(true);
+            return true;
         }
         else
         {
-            stsConf.setCategoriesBroken(false);
+            return false;
         }
-
-        return Results.redirect(context.getContextPath() + "/" + locale + "/ok3ok2ru8udqx7gZGS9n/statusInfo");
     }
 }
