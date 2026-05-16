0d6076e20d7f89687a3fde1e26ded78bd691c556
Rene Schwietzke <r.schwietzke@xceptance.com>
2026-05-16 22:17:53 +0200

    Test updates

--- test-automation/src/test/java/com/xceptance/posters/search/TC_SRC_010_KeyboardNav.java
+++ test-automation/src/test/java/com/xceptance/posters/search/TC_SRC_010_KeyboardNav.java
@@ -2,6 +2,7 @@ package com.xceptance.posters.search;
 
 import com.xceptance.neodymium.common.browser.Browser;
 import com.xceptance.neodymium.common.testdata.DataFile;
+import com.xceptance.neodymium.common.testdata.DataSet;
 import com.xceptance.neodymium.junit5.NeodymiumTest;
 import com.xceptance.neodymium.util.Neodymium;
 
@@ -23,11 +24,57 @@ import io.qameta.allure.SeverityLevel;
 @Tag("full")
 public class TC_SRC_010_KeyboardNav
 {
-    @NeodymiumTest
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Verify that a user can fully utilize the search functionality using only a keyboard.")
-    void executePosterTest() throws Throwable
+    /**
+     * Common execution logic for the test.
+     * @throws Throwable if an error occurs
+     */
+    private final void executeTest() throws Throwable
     {
         Neodymium.ai().execute();
+    }
+
+    @NeodymiumTest
+    @DataSet(id = "tc_src_010_en_US")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that a user can fully utilize the search functionality using only a keyboard (en-US).")
+    final void testKeyboardNavEnUS() throws Throwable
+    {
+        executeTest();
+    }
+
+    @NeodymiumTest
+    @DataSet(id = "tc_src_010_en_GB")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that a user can fully utilize the search functionality using only a keyboard (en-GB).")
+    final void testKeyboardNavEnGB() throws Throwable
+    {
+        executeTest();
+    }
+
+    @NeodymiumTest
+    @DataSet(id = "tc_src_010_de_DE")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that a user can fully utilize the search functionality using only a keyboard (de-DE).")
+    final void testKeyboardNavDeDE() throws Throwable
+    {
+        executeTest();
+    }
+
+    @NeodymiumTest
+    @DataSet(id = "tc_src_010_sv_SE")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that a user can fully utilize the search functionality using only a keyboard (sv-SE).")
+    final void testKeyboardNavSvSE() throws Throwable
+    {
+        executeTest();
+    }
+
+    @NeodymiumTest
+    @DataSet(id = "tc_src_010_ja_JP")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that a user can fully utilize the search functionality using only a keyboard (ja-JP).")
+    final void testKeyboardNavJaJP() throws Throwable
+    {
+        executeTest();
     }
 }
