59919771842fd06acb83d206a34c81362821b691
Rene Schwietzke <r.schwietzke@xceptance.com>
2026-05-21 00:32:45 +0200

    feat(visual): add freeform visual verification test and recorded playbooks

---
+++ test-automation/src/test/java/com/xceptance/posters/freeform/TC_FF_001_FreeformTest.java
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Xceptance Software Technologies GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+// AI-generated: Antigravity (Gemini 2.5 Pro)
+package com.xceptance.posters.freeform;
+
+import com.xceptance.neodymium.common.browser.Browser;
+import com.xceptance.neodymium.common.testdata.DataFile;
+import com.xceptance.neodymium.common.testdata.DataSet;
+import com.xceptance.neodymium.junit5.NeodymiumTest;
+import com.xceptance.neodymium.util.Neodymium;
+
+import org.junit.jupiter.api.Tag;
+
+/**
+ * A base freeform test case template designed for running experimental
+ * automated
+ * tests without pre-defined scenarios.
+ */
+@Browser("Chrome_1500x1000")
+@Tag("freeform")
+public class TC_FF_001_FreeformTest
+{
+    /**
+     * Just demo simple things
+     *
+     * @throws Throwable if execution fails
+     */
+    @NeodymiumTest
+    final void testFreeform() throws Throwable
+    {
+        Neodymium.ai().steps("""
+                # This is just a freeform test for some simple features of the automation.
+                # It is not intended to test any business functionality as defined in our test cases.
+                Open ${neodymium.url}
+                # This comes up with a verification for some critereria, it is not visual!
+                Verify that you see an ecommerce store homepage, which sells posters.
+                # Try to check for the bear.
+                Verify that a brown bear photo is show on the right side of the homepage (visual).
+                Check that the page is mainly white and blue (visual).
+                """).execute();
+    }
+}
